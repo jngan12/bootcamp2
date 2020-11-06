b06ada33ec27d27517ae26d3fa3c94f6e323c57e
zhua-intel <z.hua@intel.com>
2020-11-05 22:00:56 -0700

    Wk6 team project submission

--- lane_keeping/README.md
+++ lane_keeping/README.md
@@ -1,8 +1,43 @@
-Advanced Lane Keeping:
+# Lane Keeping
+In this project we implement a lane keeping controller by utilizing OpenCV to process the lane track information from video feed and keep the race car on the lance through the course. <br/>
+![Lane Keeping screenshot](./imgs/sae_robotics_bootcamp_wk6_lane_keeping.png)
+
+## Team: Group 1
+**Members:**
+* Atul Hatalkar
+* James Ngan
+* Robert Aguirre
+* Scott Cox
+* Vinodkumar Mudaliar
+* Zhijian Hua
+
+## Note:
+This is Team Group 1's submission for week 6's team project: lane keeping.
+
+## How to run the code
+Please follow the steps below to run the code:
+1. Clone the code repository into ROS WS on host: **git clone  https://github.com/zhua-intel/bootcamp-assignments.git**
+2. Build ROS projects if not already done so: **catkin_make**
+3. Run launch file: **roslaunch lane_keeping lane_keep.launch**
 
 
+## Design Considerations
+1. To start the car properly, the race car is placed on a specific track location by the launch file. Without taking this step the problem will have added complexity of how to navigate the car in between the tracks.
+2. The camera video feed is used as the source for video processing. The raw video frame will be passed through an OpenCV pipeline that first extract the track edges using canny detector, then crop the resulting edge image and feed it through Hough transformation. A set of line endpoints from the transformation is then  used to generate two types of errors: slope error which indicates the cars bearing against the track direction, as well as distance-to-center error which indicates the distance from the car to the center lane.
+3. The total error is calculated by combining both the slope error and distance-to-center error. Each error is weighted by its own coefficient (keslope/kecenter) to adjust its contribution to the total error. <br/>
+![Lane Keeping Error Calculation](./imgs/lane_keeping_error_calc_scheme.png)<br/>
+5. The total error is then fed to a simple P-Controller to control the linear and angular velocity of the car.
+ 
 
-Remaining Tasks:
+## Results
+Video recording for a complete lane keeping run around the course can be found here: <br/>
+**./videos/sae_robotics_bootcamp_wk6_lane_keeping.mp4**<br/>
 
-* code cleanup and commenting
-* Car needs to teleop to for code to start working. Change vehicle spawn position in a launch file for this task so that it spawns on the track itself. 
+
+## Controller Evaluation
+The controller did quite a good job guiding the car through the course while keeping it on the lane, as the video clips show. 
+
+## Reflections
+1. As mentioned earlier, the total error consists of contributions from both slope and distance-to-center errors. By adjusting the weights we can observe the effect from each of the errors. In fact we observed that the car can navigate successfully using only the slope error. One reason is perhaps that the car is placed on the right spot on the lane to begin with, and the overall gentle curvature of the lane makes it possible to navigate with only one type of error input. 
+
+
--- lane_keeping/scripts/simple_lane_keep.py
+++ lane_keeping/scripts/simple_lane_keep.py
@@ -15,91 +15,193 @@ import math
 steering_angle = 0
 velocity = 0.3 # (m/s)
 
-
-def canny(image):
-    # TO-DO: Extract the canny lines
-    # ---
-
-    # ---
-    return canny
-
-def region_of_interest(image):
-    triangle = np.array([[(0, 480), (0, 288), (639, 288), (639, 480)]])
-    # TO:DO Find the  Region of Interest
-    # ---
-
-    # ---
-    return masked_image
+# Self defined global variables
 
 
-def average_slope_intercept(image, lines):
-    # TO-DO: Get and average of the left and right Hough Lines and extract the centerline. 
-    # The angle between the extracted centerline and desired centerline will be the error. 
-    # Use cv2.line to display the lines.
-    # ---
+class laneKeeping(object):
+    def __init__(self):
 
-    # ---
+        self.pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=1)
+        self.bridge_object = CvBridge()
+	self.image_sub = rospy.Subscriber("/camera/zed/rgb/image_rect_color", Image, self.camera_callback)
+        # Give some initial velocity to make the robot move
+        self.linear  = 1
+        self.angular = 0
+        # Coefficients 
+        self.keslope  = 0.9 # Slope error contribution to overall error
+        self.kecenter = 0.1 # Distance to center contribution to overall error
+        self.kp       = 1.0 # P controller coefficients
 
-def camera_callback(data):
+    def p_controller(self, error):
+        if error != 0:
+            self.linear = 0.3
+            self.angular = self.kp * error
+        else:
+            self.linear = 1
+            self.angular = 0
+
+        return 
+
+    # Controlls robot movement
+    def movement(self, linear_velocity, angular_velocity):
+
+        msg = AckermannDriveStamped()
+        msg.drive.speed = linear_velocity
+        msg.drive.steering_angle = angular_velocity 
+        self.pub.publish(msg)
+
+        return
+
+    def canny(self, image):
+        # TO-DO: Extract the canny lines
+        # ---
+        canny_img = cv2.Canny(image, image.max(), image.min())
+
+        # ---
+        return canny_img
+
+    def region_of_interest(self, image):
+        triangle = np.array([[(0, 480), (0, 288), (639, 288), (639, 480)]])
+        # TO:DO Find the  Region of Interest
+        # ---
+        masked_image = image[triangle[0][1][1]:triangle[0][0][1], triangle[0][0][0]:triangle[0][2][0]]        
+        # ---
+        return masked_image
+
+
+    def average_slope_intercept(self, image, lines):
+        # TO-DO: Get and average of the left and right Hough Lines and extract the centerline. 
+        # The angle between the extracted centerline and desired centerline will be the error. 
+        # Use cv2.line to display the lines.
+        # ---
+        eslope  = 0
+        ecenter = 0
+
+        # Array storing the lane slopes 
+        slope = []
+        avg_center = [0, 0]
+
+        try:
+            for line in lines:
+                print line
+                for x1,y1,x2,y2 in line:
+                    image = cv2.line(image, (x1, y1), (x2, y2), (0,255,0), 4 )
+                    # Calculate slope 
+                    if x1 == x2:
+                        slope.append(float("inf"))
+                    else:
+                        slope.append( 1.0*(y2-y1)/(x2-x1) )
+                    avg_center[0] = (avg_center[0] +  (x1+x2)/2)/2
+                    avg_center[1] = (avg_center[1] +  (y1+y2)/2)/2
+        except:
+            avg_center[0] = 320
+            avg_center[1] = 0
+            pass
+
+        #cv_cropped = cv2.circle(cv_cropped, (avg_center[0], avg_center[1]), 4, (0,255,0), 4 )
+       
+        # Calculate slope error
+        if len(slope) != 0:
+            eslope = sum(slope)/len(slope)
+        else:
+            eslope = 0
+
+        # Calculate normalized distance-to-center error
+        ecenter = (avg_center[0] - 320) * math.pi/(180 * 640)
+
+        # Calculate final error
+        error = self.keslope * eslope + self.kecenter * ecenter
+
+        # ---
+        return error, image
+
+    def camera_callback(self, data):
     
-    global steering_angle
-    global velocity
-    
-    # TO-DO: Convert the ROS Image to CV type.
-    # ---
-    cv_image = 
-    # ---
+        global steering_angle
+        global velocity
 
-    # TO-DO: Extract the canny lines
-    canny_image = canny(cv_image)
+        # TO-DO: Convert the ROS Image to CV type.
+        # ---
+        cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="passthrough") 
+        # ---
 
-    # TO:DO Find the  Region of Interest
-    cropped_image = region_of_interest(canny_image)
+        # TO-DO: Extract the canny lines
+        canny_image = self.canny(cv_image)
 
-    # Extract the Hough Lines
-    lines = cv2.HoughLinesP(# --   --- #)
+        # TO:DO Find the  Region of Interest
+        cropped_image = self.region_of_interest(canny_image)
 
-    # TO-DO: Get and average of the left and right Hough Lines and extract the centerline. 
-    # The angle between the extracted centerline and desired centerline will be the error. 
-    # Use cv2.line to display the lines.
-    averaged_lines = average_slope_intercept(cv_image, lines)
+        # Extract the Hough Lines
+        rho = 1
+        theta = np.pi/180
+        threshold = 10
+        minLineLength = 100
+        maxLineGap = 20
+        lines = cv2.HoughLinesP(cropped_image, rho, theta, threshold, minLineLength, maxLineGap)
+        cv_cropped = self.region_of_interest(cv_image)
 
-    # TO-DO: Implement the final controller
-    # ---
+        # TO-DO: Get and average of the left and right Hough Lines and extract the centerline. 
+        # The angle between the extracted centerline and desired centerline will be the error. 
+        # Use cv2.line to display the lines.
+        error, cv_cropped = self.average_slope_intercept(cv_cropped, lines)
 
-    # ---
-    
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown():
-        # TO-DO: Publish the steering angle and velocity
+        # TO-DO: Implement the final controller
         # ---
 
         # ---
+        self.p_controller(error)
 
-        vel.header.stamp = rospy.Time.now()
-        vel.header.frame_id = "base_link"
+        cv2.imshow('canny', canny_image)
+        cv2.imshow('ROI', cv_cropped)
+        cv2.waitKey(1)
 
-        print("Steering angle: %f" % m)
-        pub.publish(vel)
+        # Move the robot
+        self.movement(self.linear, self.angular)
+        return
 
-        rate.sleep()
+        rate = rospy.Rate(10)
 
-    pub.publish(vel)
+        while not rospy.is_shutdown():
+            # TO-DO: Publish the steering angle and velocity
+            # ---
 
-    # Display converted images
-    cv2.imshow('canny',canny_image)
-    cv2.imshow('ROI',cropped_image)
-    cv2.waitKey(1)
+            # ---
 
+            vel.header.stamp = rospy.Time.now()
+            vel.header.frame_id = "base_link"
+
+            print("Steering angle: %f" % m)
+            self.pub.publish(vel)
+
+            rate.sleep()
+
+        self.pub.publish(vel)
+
+        # Display converted images
+        cv2.imshow('canny',canny_image)
+        cv2.imshow('ROI',cropped_image)
+        cv2.waitKey(1)
+
+def main():
+    rospy.init_node('lane_keeping', anonymous=True)
+    lane_keeping_obj = laneKeeping()
+
+    #bridge_object = CvBridge()
+
+    # TO-DO: Publish and subscribe to the correct topics.
+    # ---
+
+    # ---
+    #pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=1)
+    #rospy.Subscriber("/scan", LaserScan, camera_callback)
+    #vel = AckermannDriveStamped()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
 
 if __name__ == '__main__':
-    rospy.init_node('lane_keeping')
-    
-    bridge_object = CvBridge()
+    main()
 
-    # TO-DO: Publish and subscribe to the correct topics. 
-    # ---
 
-    # ---
-    vel = AckermannDriveStamped()
+
